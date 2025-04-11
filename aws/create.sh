c30b11b1afcdaa5e14b6e0681dfcda19f9ab05df
Adam Boczek <adam.boczek@nttdata.com>
2025-04-11 12:46:21 +0200

    feat: add shell commands for temp docs

--- aws/create.sh
+++ aws/create.sh
@@ -5,3 +5,11 @@ aws iam create-policy --no-verify-ssl --policy-name rds-operator-policy --policy
 eksctl utils associate-iam-oidc-provider --cluster aboczek-test-cluster-manu3 --approve
 
 eksctl create iamserviceaccount --name aws-rds-operator --namespace k8s-operator-aws-rds-demo-system --cluster aboczek-test-cluster-manu3 --attach-policy-arn arn:aws:iam::063010046250:policy/rds-operator-policy --approve
+
+make generate
+make manifests
+
+make docker-build docker-push IMG="063010046250.dkr.ecr.eu-central-1.amazonaws.com/k8s-operator-aws-rds-demo:v0.0.18"
+make deploy IMG="063010046250.dkr.ecr.eu-central-1.amazonaws.com/k8s-operator-aws-rds-demo:v0.0.18"
+
+kubectl apply -f config/samples/aws_v1alpha1_awsrdsdemoinstance.yaml
